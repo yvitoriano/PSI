6952be8f2f7264216e55c926c997e25172f51ed1
Yasmin da Silva Vitoriano Medeiros <20231041110021@ifrn.local>
2025-07-03 17:28:44 -0300

    Aula sobre Javascript

---
+++ estudos_javascript.js
@@ -0,0 +1,43 @@
+console.log("Cupcake..."); //Parecido com print em python 
+//Variáveis :
+/* let - Para as variáveis que possam ser mudadas
+const - Não mudam */
+
+let nome = "Yasmin"
+nome = "Vitoriano"
+console.log(nome)
+
+const idade = 77
+console.log(idade)
+
+// Listas ["Yasmin", "Vitoriano"]
+/*console.log(lista[1])*/
+
+//Condicionais
+// === } mesmo nome e mesmo tipo /    == } mesmo nome 
+if (nome === "Vitoriano") {
+    console.log("É seu nome ")
+} else{
+    console.log("Não é o meu nome")
+};
+
+
+//Repetições : FOR e WHILE
+
+/* Funções  
+function oi(nome){
+   console.log("Olá" + nome)*/ 
+
+function oi (nome){
+    console.log(`Olá ${nome}! Você tem ${idade}`)
+}
+
+oi("Vitoriano")
+
+//Classes:
+
+let livro = {
+    nome: "Pequeno Príncipe"
+}
+
+console.log(livro.nome)
